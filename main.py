961490b1462cbd64e2f256b54d87503e090e7162
edumgoulart <edugoulart22@gmail.com>
2025-07-08 14:53:51 -0300

    getting data

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
 
-all_currencies = requests.get("https://api.frankfurter.dev/v1/currencies")
-print(all_currencies.json())
+#all_currencies = requests.get("https://api.frankfurter.dev/v1/currencies")
+#print(all_currencies.json())
+
+curr_in = input("Type the currency you have:").upper()
+
+curr_out = input("Type the currency you want to convert:").upper()
+
+amount = input(f"Type how much {curr_in} you want to convert:")
+
+data = requests.get(f"https://api.frankfurter.dev/v1/latest?base={curr_in}&symbols={curr_out}")
+
+print(data.json())
