1bb7a39fe1a7173dc71da3fe0c4f083bc35e9b0d
edumgoulart <edugoulart22@gmail.com>
2025-07-08 21:15:59 -0300

    Core features working

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ curr_in = input("Type the currency you have:").upper()
 
 curr_out = input("Type the currency you want to convert:").upper()
 
-amount = input(f"Type how much {curr_in} you want to convert:")
+amount = int(input(f"Type how much {curr_in} you want to convert:"))
 
+#gets the value of the conversion rate
 data = requests.get(f"https://api.frankfurter.dev/v1/latest?base={curr_in}&symbols={curr_out}")
 
-print(data.json())
+conv_rate = data.json()["rates"][curr_out]
+
+conv_amount = round(amount * conv_rate, 2)
+
+print(f"Yours {amount} {curr_in} will be {conv_amount} {curr_out}.")
+
+
